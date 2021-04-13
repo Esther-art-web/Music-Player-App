fd244ec23414306016d108a58e421fbd413cff5c
Ibeh Esther <estheribeh5@gmail.com>
2021-04-13 22:40:06 +0100

    made some changes to html and css files

--- script.js
+++ script.js
@@ -23,22 +23,22 @@ let image_cover_bg= document.getElementById("image-cover-bg");
 let music_title= document.getElementById("title");
 let music_artiste = document.getElementById("artiste");
 
-const playlist_details = {'Minden': "Love is Bad",
-                         '6lack ft jcole' : "Pretty Little Fears",
-                         'jhene' : "Love",
-                         'Lewis Capaldi' : "Hollywood",
-                         'Ariana Grande' : "pov",
-                         'Pink Sweats ft Kehlani': "At my worst",
-                         'Summer Walker ft Drake' : "Girls need love",
-                         'Alessia Cara' : "Out of Love"}
-const playlist_songs = ["6lack_pretty_little_fears",
-                         "jhene_love", 
-                         "lewis_hollywood", 
-                         "ariana_pov",
-                         "pink_at_my_worst",
-                         "summer_girls_need_love",
-                         "alessia_out_of_love"
-                        ]
+const playlist_details = {'Minden': ["Love is Bad", 2],
+                         '6lack ft jcole' : ["Pretty Little Fears","6lack_pretty_little_fears"],
+                         'jhene' : ["Love", "jhene_love" ],
+                         'Lewis Capaldi' :["Hollywood", "lewis_hollywood"],
+                         'Ariana Grande' : ["pov","ariana_pov" ],
+                         'Pink Sweats ft Kehlani': ["At my worst","pink_at_my_worst"],
+                         'Summer Walker ft Drake' : ["Girls need love", "summer_girls_need_love" ],
+                         'Alessia Cara' : ["Out of Love", "alessia_out_of_love"]}
+// const playlist_songs = ["6lack_pretty_little_fears",
+//                          "jhene_love", 
+//                          "lewis_hollywood", 
+//                          "ariana_pov",
+//                          "pink_at_my_worst",
+//                          "summer_girls_need_love",
+//                          "alessia_out_of_love"
+//                         ]
 
 const playSong = (audio) =>{
     image_cover.classList.add('play');
@@ -50,6 +50,19 @@ const playSong = (audio) =>{
     image_cover_bg.src=`${audio}.jpg`;
     
 }
+const pauseSong=()=>{
+    image_cover.classList.remove('play');
+    play_icon.classList.add('fa-play');
+    play_icon.classList.remove('fa-pause');
+   
+}
+const toogleSong =(audio)=>{
+    if (play_icon.classList.contains("fa-play")){
+        playSong(audio);
+    }else{
+        pauseSong();
+    }
+}
 
 const musicTabClick =(name, artiste, audio)=>{
     music_playing_name.innerHTML= name;
@@ -58,4 +71,16 @@ const musicTabClick =(name, artiste, audio)=>{
     music_artiste.innerHTML= artiste;
     image_cover.src= `${audio}.jpg`;
     playSong(audio);
+}
+const forwardSong =()=>{
+    for (artiste in playlist_details){
+        // console.log(playlist_details[artiste][1]);
+        if (music_playing_artiste.innerHTML === artiste){
+            let music= playlist_details[artiste][0];
+            let audio= playlist_details[artiste][1];
+            musicTabClick(music, artiste, audio);
+            console.log('Hello');
+            break;
+        }
+    }
 }
